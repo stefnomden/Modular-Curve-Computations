40981004896bcdcfd656c7bbc14d3e0ac7837944
stefnomden <133787190+stefnomden@users.noreply.github.com>
2023-05-26 12:07:01 +0200

    Update N=15.py

--- carried_out_computations/N=15.py
+++ carried_out_computations/N=15.py
@@ -7,25 +7,25 @@ M = np.array([[-17,-4],[30,7]])
 vplus = [0,1]
 vminus = [1,0]
 gamma = np.array([-1,1])
-expected_j = 3**(-4)*5**(-4)*13**(3)*37**(3)
 values = coeffs_calculator(eta_product15,M,vplus,vminus,gamma)
 print('ω1=',values[3])
 print('ω2=',values[4])
-print('ω1/ω2=',values[3]/values[4])
 print('g4=', values[0])
 print('g6=', values[1])
-c4 = 12*values[0]/((2*math.pi)**4)
-c6 = -216*values[1]/((2*math.pi)**6)
+c4 = 12*values[0]
+c6 = 216*values[1]
 c4guess = round(c4.real)
 c6guess = round(c6.real)
 print('c4guess=',c4guess)
 print('c6guess=',c6guess)
 u = u_calculator(c4guess, c6guess)
-print('u=',u)
 c4prime = c4guess/(u**4)
 c6prime = c6guess/(u**6)
 a = a_calculator(c4prime, c6prime)
-print('coeffs:',a)
+delta = (c4prime**3 - c6prime**2)/1728
+expected_j = (c4prime**3)/delta
+print('coeffs:',a)                    #order is (a_1,a_3,a_2,a_4,a_6)
+print('Δ=',delta,'=',prime_decomp(delta))
 print('j=', values[2])
 print('expected j=',expected_j)
 print('error=',abs(expected_j-values[2]))
